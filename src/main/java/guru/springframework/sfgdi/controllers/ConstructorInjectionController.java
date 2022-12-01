91f1d9a36fdb12d4f34279f648cae14d4e1189f0
Nicolas Raigosa <nicolasraigosa@gmail.com>
2022-12-01 16:48:34 -0500

    Dependency Injection without Spring

---
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectionController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+
+public class ConstructorInjectionController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectionController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.SayGreeting();
+    }
+}
