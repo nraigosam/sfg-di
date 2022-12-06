c3711204c71cd80166d716e4cdf29aeea17f7026
Nicolas Raigosa <nicolasraigosa@gmail.com>
2022-12-06 13:23:30 -0500

    Merge branch 'di-assignment' into di-assignment-review
    # Conflicts:
    #	.gitignore
    #	mvnw
    #	mvnw.cmd
    #	pom.xml
    #	src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
    #	src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
    #	src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
    #	src/main/java/guru/springframework/sfgdi/controllers/MyController.java
    #	src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
    #	src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
    #	src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
    #	src/main/java/guru/springframework/sfgdi/services/GreetingService.java
    #	src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
    #	src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
    #	src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
    #	src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
    #	src/main/resources/application.properties
    #	src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
    #	src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
    #	src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
    #	src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -8,12 +8,16 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SfgDiApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-        System.out.println(i18nController.sayHello());
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
         MyController myController = (MyController) ctx.getBean("myController");
 
