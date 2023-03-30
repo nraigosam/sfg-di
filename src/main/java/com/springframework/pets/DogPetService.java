09cfc0433f7c4b0d3dfce8a61d7fc66e3004eea2
Nicolas Raigosa <nicolasraigosa@gmail.com>
2023-03-30 16:38:14 -0500

    adding factory bean example

--- src/main/java/com/springframework/pets/DogPetService.java
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -1,13 +1,11 @@
 package com.springframework.pets;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Service()
-@Profile({"dog", "default"})
+//@Service()
+//@Profile({"dog", "default"})
 public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
